5af9a83177de49e093541171af609e3567de1399
Shahinaz Elmahmoudi <sbloi001@fiu.edu>
2017-12-05 17:51:00 -0500

    Learning about Event Handlers

--- Exercise Files/07_05/JS/script.js
+++ Exercise Files/07_05/JS/script.js
@@ -4,11 +4,15 @@ const ALERT = document.querySelector("#booking-alert");
 CTA.classList.remove("hide");
 ALERT.classList.add("hide");
 
-function reveal(e) {
+function reveal(e, current) {
     e.preventDefault();
-    CTA.classList.toggle("hide");
+
+    current.innerHTML == "Book Now!" ? CTA.innerHTML = "Ooooops!" : CTA.innerHTML = "Book Now!";
+
     ALERT.classList.toggle("hide");
 }
 
-CTA.addEventListener('click', reveal, false);
+CTA.addEventListener('click', function (e) {
+    reveal(e, this);
+}, false);
 CTA.addEventListener('click', function(){console.log("The button was clicked!")}, false);
